b036b91cb4a2a34630e1405e14d27a4816a1ab4a
Pavloffff <pavlov.id.2003@gmail.com>
2023-03-27 21:14:24 +0300

    front refactor & gitignore

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -1,12 +1,13 @@
 const VideoPlayer = ({ url }) => {
 	return (
-		<iframe
-			src={url}
-			allow='accelerometer, autoplay; encrypted-media; gyroscope; picture-in-picture'
-			allowFullScreen
-			width='1920'
-			height='1080'
-		></iframe>
+		// <iframe
+		// 	src={url}
+		// 	allow='accelerometer, autoplay; encrypted-media; gyroscope; picture-in-picture'
+		// 	allowFullScreen
+		// 	width='1920'
+		// 	height='1080'
+		// ></iframe>
+        <img src="http://localhost:5011/video"></img>
 	)
 }
 
