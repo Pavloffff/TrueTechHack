2993cc6066bc128109e74fce9cae721a4292be5d
crewch <artemkr2003@mail.ru>
2023-03-28 15:05:21 +0300

    fix

--- frontend/src/screens/FilmScreen.jsx
+++ frontend/src/screens/FilmScreen.jsx
@@ -1,17 +1,22 @@
 import { useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
 import VideoPlayer from '../components/VideoPlayer'
 import { services } from '../services/services'
 import styles from '../styles/FilmScreen.module.scss'
 
 const FilmScreen = () => {
-	const { data, isLoading, isSuccess, isError, error } = useQuery({
-		queryKey: ['getPort'],
-		queryFn: () =>
-			services.postUser({
-				login: JSON.parse(localStorage.getItem('login')).login,
-				filmName: '806.mp4',
+	const { data, isLoading, isSuccess, isError, error } = useMemo(
+		() =>
+			useQuery({
+				queryKey: ['getPort'],
+				queryFn: () =>
+					services.postUser({
+						login: JSON.parse(localStorage.getItem('login')).login,
+						filmName: '806.mp4',
+					}),
 			}),
-	})
+		[]
+	)
 
 	return (
 		<div className={styles.content}>
