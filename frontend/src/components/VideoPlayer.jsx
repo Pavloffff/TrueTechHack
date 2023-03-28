99f788876fc553f07a59f11b55c05a62eb4660f8
crewch <artemkr2003@mail.ru>
2023-03-28 17:20:15 +0300

    fix

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query'
 import { services } from '../services/services'
 
-const VideoPlayer = ({ port }) => {
+const VideoPlayer = () => {
 	const mutation = useMutation({
 		mutationFn: () =>
 			services.start({
@@ -9,16 +9,25 @@ const VideoPlayer = ({ port }) => {
 			}),
 	})
 
+	const { data, isLoading, isSuccess, isError, error } = useQuery({
+		queryKey: ['getPort'],
+		queryFn: () =>
+			services.postUser({
+				login: JSON.parse(localStorage.getItem('login')).login,
+				filmName: '806.mp4',
+			}),
+		onSuccess: () => {
+			mutation.mutate()
+		},
+		refetchOnWindowFocus: false,
+	})
+
 	return (
 		<div>
-			<img src={`http://localhost:${port}/video`} />
-			<button
-				onClick={() => {
-					mutation.mutate()
-				}}
-			>
-				start
-			</button>
+			{isLoading && <h1>Loading...</h1>}
+			{isError && <h1>{error.message}</h1>}
+			{isSuccess && <img src={`http://localhost:${data}/video`} />}
+			{console.log('PORT:' + data)}
 		</div>
 	)
 }
--- frontend/src/screens/FilmScreen.jsx
+++ frontend/src/screens/FilmScreen.jsx
@@ -1,38 +1,10 @@
-import { useQuery } from '@tanstack/react-query'
-import { useMutation } from '@tanstack/react-query'
-import { services } from '../services/services'
+import VideoPlayer from '../components/VideoPlayer'
 import styles from '../styles/FilmScreen.module.scss'
 
 const FilmScreen = () => {
-	const mutation = useMutation({
-		mutationFn: () =>
-			services.start({
-				login: JSON.parse(localStorage.getItem('login')).login,
-			}),
-	})
-
-	const { data, isLoading, isSuccess, isError, error } = useQuery({
-		queryKey: ['getPort'],
-		queryFn: () =>
-			services.postUser({
-				login: JSON.parse(localStorage.getItem('login')).login,
-				filmName: '806.mp4',
-			}),
-		onSuccess: () => {
-			mutation.mutate()
-		},
-		refetchOnWindowFocus: false,
-	})
-
 	return (
 		<div className={styles.content}>
-			{isLoading && <h1>Loading...</h1>}
-			{isError && <h1>{error.message}</h1>}
-			{isSuccess && (
-				<div>
-					<img src={`http://localhost:${data}/video`} />
-				</div>
-			)}
+			<VideoPlayer />
 		</div>
 	)
 }
